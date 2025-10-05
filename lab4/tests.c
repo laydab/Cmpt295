5408429df5de9124dd7bebf9ef4b18f9a65c886e
Ayden Badyal <aydenbadyal@gmail.com>
2025-10-05 14:26:07 -0700

    lab4

--- lab4/tests.c
+++ lab4/tests.c
@@ -4,15 +4,17 @@
 
 int main() {
     int64_t two62 = 1L<<62;  // == 2^62
+
     printf("is_prime(2) == 1 == %lu\n", is_prime(2));
     printf("is_prime(6) == 0 == %lu\n", is_prime(6));
     printf("is_prime(7) == 1 == %lu\n", is_prime(7));
     printf("is_prime(3083) == 1 == %lu\n", is_prime(3083));
     printf("is_prime(1472741) == 0 == %lu\n", is_prime(1472741));
+
     
     printf("largest_power_unsigned(3) == 12157665459056928801 == %lu\n", largest_power_unsigned(3));
     printf("largest_power_unsigned(7) == 3909821048582988049 == %lu\n", largest_power_unsigned(7));
-    
+
     printf("overflowing_subtract(10, 3) == 7 == %li\n", overflowing_subtract(10, 3));
     printf("overflowing_subtract(two62, two62-3) == 3 == %li\n", overflowing_subtract(two62, two62-3));
     printf("overflowing_subtract(-two62, two62) == -9223372036854775808 == %li\n", overflowing_subtract(-two62, two62));
