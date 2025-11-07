b92cdd963be31c1afe333f9b9c753b98c16c1782
Ayden Badyal <aydenbadyal@gmail.com>
2025-11-07 10:47:50 -0800

    replaced code from lab9 to this for lab 9 question

--- lab8/array-2d.c
+++ lab8/array-2d.c
@@ -66,45 +66,34 @@ time_result* time_it(DATA_T(*sum_array)(DATA_T*, uint64_t, uint64_t), DATA_T* ar
     return res;
 }
 
+// This is a replacement main for your array-2d.c...
+
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        printf("Must give number of array elements on command line.\n");
-        return 3;
+    if (argc < 3) {
+        printf("Must give array size and height on command line.\n");
+        return 1;
     }
     uint64_t n = atol(argv[1]);
-    printf("Array size: %lukB\n", n * sizeof(DATA_T) / 1024);
+    uint64_t h = atol(argv[2]);
+    printf("Array size: %lu kB\n", n * sizeof(DATA_T) / 1024);
+    
+    if ( n % h != 0 ) {
+        printf("n not divisible by h\n");
+        return 1;
+    }
 
     DATA_T* array = create_array(n);
     if (array == NULL) {
-        printf("Couldn't allocate array.\n");
-        return 4;
+        printf("Couldn't allocate.\n");
+        return 2;
     }
     time_result* res;
 
-    // do some warmups
-    res = time_it(sum_array_row, array, n, 1);
+    uint64_t w = n / h;
+    // treat array like it's h*w...
+    res = time_it(sum_array_col, array, w, h);
+    printf("Calculated " DATA_PRINTF " in %8.2f ms on %lu*%lu array.\n", res->result, res->elapsed_ms, w, h);
     free(res);
-    res = time_it(sum_array_col, array, n, 1);
-    free(res);
-    res = time_it(sum_array_row, array, n, 1);
-    free(res);
-    res = time_it(sum_array_col, array, n, 1);
-    free(res);
-
-    for (uint64_t w = 1; w <= n; w++) {
-        // try all array widths that divide the size...
-        if (n % w == 0) {
-            uint64_t h = n / w;
-            // treat array like it's h*w...
-            res = time_it(sum_array_row, array, w, h);
-            printf("Calculated row " DATA_PRINTF " in %8.2fms on %lu*%lu array.\n", res->result, res->elapsed_ms, w, h);
-            free(res);
-            res = time_it(sum_array_col, array, w, h);
-            printf("Calculated col " DATA_PRINTF " in %8.2fms on %lu*%lu array.\n", res->result, res->elapsed_ms, w, h);
-            free(res);
-        }
-    }
     free(array);
     return 0;
 }
-
